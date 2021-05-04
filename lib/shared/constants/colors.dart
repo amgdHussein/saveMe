9356db18272a62ecdc8999027175578921252f66
Amgd2112 <amgad.hussein99.com>
2021-05-04 03:06:40 +0200

    move data from const and rename

--- lib/shared/constants/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-const Color GRAY_CHATEAU = Color(0xffa2a9af);
-const Color GHARADE = Color(0xff252A34);
-const Color CONCRETE = Color(0xffebedec);
