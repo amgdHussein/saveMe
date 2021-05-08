47d7d9af6761e9519b0bf865fcf6c5d82c93c82e
Amgd2112 <amgad.hussein99.com>
2021-05-08 07:03:49 +0200

    remove files for file arch

--- lib/models/onboarding_text.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class OnboardingText {
-  final List<String> sentences;
-  final List<Color> colors;
-
-  OnboardingText({@required this.sentences, @required this.colors});
-}
