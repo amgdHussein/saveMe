ef0c1d7bcd1604a1978a06e3b15ec8eace56a06e
Amgd2112 <amgad.hussein99.com>
2021-07-03 22:32:01 +0200

    missing and finding tabs

--- lib/modules/save_me/screens/home.dart
+++ lib/modules/save_me/screens/home.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:save_me/widgets/app_logo.dart';
 import '../models/post.dart';
-import 'profile/cubit/profile_cubit.dart';
 import '../../../widgets/post/post_view.dart';
-// import 'package:save_me/widgets/posts.dart';
-import '../../../widgets/app_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:save_me/modules/save_me/repositories/post_repository.dart';
 
@@ -13,41 +10,73 @@ import 'package:save_me/modules/save_me/repositories/post_repository.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key key}) : super(key: key);
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  PostRepository _postRepo = PostRepository();
+  final PostRepository _postRepo = PostRepository();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: appBar(context, isAppTitle: true, disableBack: true),
-      body: BlocBuilder<ProfileCubit, ProfileState>(
-        builder: (context, state) {
-          return StreamBuilder<QuerySnapshot>(
-            stream: _postRepo.posts,
-            builder: (
-              BuildContext context,
-              AsyncSnapshot<QuerySnapshot> snapshot,
-            ) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return Center(child: CircularProgressIndicator());
-                default:
-                  if (snapshot.hasError)
-                    return Text('Error: ${snapshot.error}');
-                  else {
-                    return viewPosts(
-                      context: context,
-                      posts: snapshot.data.docs.map((doc) {
-                        Map<String, dynamic> postMap = doc.data();
-                        if (postMap.containsKey('missingFrom'))
-                          return Missing.fromMap(postMap);
-                        return Finding.fromMap(postMap);
-                      }).toList(),
-                    );
-                  }
-              }
-            },
-          );
-        },
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          automaticallyImplyLeading: true,
+          leadingWidth: 30,
+          title: logoRichText(
+            textStyle1: Theme.of(context).appBarTheme.textTheme.headline1,
+            textStyle2: Theme.of(context).appBarTheme.textTheme.headline2,
+          ),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(text: "Missing"),
+              Tab(text: "Finding"),
+            ],
+          ),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: _postRepo.posts,
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot,
+          ) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              default:
+                if (snapshot.hasError)
+                  return Text('Error: ${snapshot.error}');
+                else {
+                  return TabBarView(
+                    children: [
+                      viewPosts(
+                        context: context,
+                        posts: snapshot.data.docs
+                            .where(
+                              (post) {
+                                Map<String, dynamic> map = post.data();
+                                return map.containsKey('missingFrom');
+                              },
+                            )
+                            .map((e) => Missing.fromMap(e.data()))
+                            .toList(),
+                      ),
+                      viewPosts(
+                        context: context,
+                        posts: snapshot.data.docs
+                            .where(
+                              (post) {
+                                Map<String, dynamic> map = post.data();
+                                return (!map.containsKey('missingFrom'));
+                              },
+                            )
+                            .map((e) => Finding.fromMap(e.data()))
+                            .toList(),
+                      ),
+                    ],
+                  );
+                }
+            }
+          },
+        ),
       ),
     );
   }
